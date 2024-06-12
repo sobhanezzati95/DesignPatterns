e7488d4b1104c60c8491994b16964858fa4d2f6a
Sobhan Ezzati <SobhanEzzati95@gmail.com>
2024-06-12 11:34:22 +0330

    Singleton Sample

---
+++ Design_Patterns/Creational-Patterns/Singleton/LaziInitializerSingleton.cs
@@ -0,0 +1,13 @@
+﻿namespace Design_Patterns.Creational_Patterns.Singleton
+{
+    public class LaziInitializerSingleton
+    {
+        private readonly static Lazy<LaziInitializerSingleton> _uniqueInstance = new Lazy<LaziInitializerSingleton>();
+        private LaziInitializerSingleton() { }
+
+        public static LaziInitializerSingleton GetInstance()
+        {
+            return _uniqueInstance.Value;
+        }
+    }
+}
---
+++ Design_Patterns/Creational-Patterns/Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿namespace Design_Patterns.Creational_Patterns.Singleton
+{
+    public class Singleton
+    {
+        private static readonly Singleton _uniqueInstance = new Singleton();
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            return _uniqueInstance;
+        }
+
+        //internal class Program
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        var obj1 = Singleton.GetInstance();
+        //        var obj2 = Singleton.GetInstance();
+
+        //        Console.WriteLine(obj1 == obj2);
+
+        //    }
+        //}
+    }
+}
