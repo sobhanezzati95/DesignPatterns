f546d84aa46c9ff1a2f355c6dd34d780a0ef223c
Sobhan Ezzati <SobhanEzzati95@gmail.com>
2024-06-18 16:12:07 +0330

    State Sample

---
+++ Design_Patterns/Behavioral-Patterns/State/State.cs
@@ -0,0 +1,59 @@
+﻿namespace Design_Patterns.Behavioral_Patterns.State
+{
+    public class Context
+    {
+        public State State { get; set; }
+        public Context()
+        {
+            State = new ConcreateState_A();
+        }
+
+        public void Request()
+        {
+            State.Handle(this);
+        }
+    }
+
+    public abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    public class ConcreateState_A : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("Request Handled By ConcreateState_A");
+            context.State = new ConcreateState_B();
+        }
+    }
+
+    public class ConcreateState_B : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("Request Handled By ConcreateState_B");
+            context.State = new ConcreateState_C();
+        }
+    }
+
+    public class ConcreateState_C : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("Request Handled By ConcreateState_C");
+            context.State = new ConcreateState_A();
+        }
+    }
+
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Context context = new Context();
+    //        context.Request();
+    //        context.Request();
+    //        context.Request();
+    //    }
+    //}
+}
