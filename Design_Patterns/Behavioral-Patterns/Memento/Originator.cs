ee89efedb9fe331320db52c0a920bcc48b737eea
Sobhan_Ezzati <sobhanezzati95@gmail.com>
2025-04-14 21:30:12 +0330

    implement Memento pattern

---
+++ Design_Patterns/Behavioral-Patterns/Memento/Originator.cs
@@ -0,0 +1,59 @@
+﻿namespace Design_Patterns.Behavioral_Patterns.Memento;
+public class Originator
+{
+    string state;
+    public string State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    public Memento CreateMemento()
+    {
+        return new Memento(state);
+    }
+    public void SetMemento(Memento memento)
+    {
+        State = memento.State;
+    }
+}
+public class Memento
+{
+    string state;
+    public Memento(string state)
+    {
+        this.state = state;
+    }
+    public string State
+    {
+        get { return state; }
+    }
+}
+public class Caretaker
+{
+    Memento memento;
+    public Memento Memento
+    {
+        get { return memento; }
+        set { memento = value; }
+    }
+}
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Originator originator = new Originator();
+//        originator.State = "Green";
+//
+//        Caretaker caretaker = new Caretaker();
+//        caretaker.Memento = originator.CreateMemento();
+//
+//        originator.State = "Red";
+//        originator.State = "Yellow";
+//        originator.State = "Blue";
+//
+//        originator.SetMemento(caretaker.Memento);
+//
+//        Console.ReadKey();
+//    }
+//}
